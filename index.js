61f5d5240aebc21e2ed81eaf770f26aa3be7002d
Erick Scur <erickscur@gmail.com>
2021-09-16 11:37:19 -0300

    added a test view to test the HTML form submition

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const connection = require('./src/database/database');
 const bodyParser = require('body-parser');
 const app = express();
 const routes = require('./src/routes');
-const Product = require('./src/models/Product');
+const Image = require('./src/models/Image');
+
 connection
     .authenticate()
     .then(() => {
@@ -15,12 +16,16 @@ connection
     
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+app.set('view engine', 'ejs');
 
 app.use(routes);
 
 app.get("/health",(req,res)=>{
     res.json({"status":"running"});
 });
+app.get('/test',(req,res)=>{
+    res.render('test');
+})
 
 app.listen(8080, ()=>{
     console.log("Servidor rodando");
